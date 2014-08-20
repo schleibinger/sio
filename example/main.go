25f637627109f167601e23312e666cf7ea202a0c
Michael Phan-Ba <michael@mikepb.com>
2014-08-19 18:05:45 -0700

    Update examples and add baud rate constants

--- example/main.go
+++ example/main.go
@@ -21,43 +21,42 @@ import (
 	"io"
 	"os"
 	"runtime"
-	"syscall"
 	"time"
 
-	"github.com/schleibinger/sio"
+	".."
 )
 
-var bauds = map[int]uint32{
-	50:      syscall.B50,
-	75:      syscall.B75,
-	110:     syscall.B110,
-	134:     syscall.B134,
-	150:     syscall.B150,
-	200:     syscall.B200,
-	300:     syscall.B300,
-	600:     syscall.B600,
-	1200:    syscall.B1200,
-	1800:    syscall.B1800,
-	2400:    syscall.B2400,
-	4800:    syscall.B4800,
-	9600:    syscall.B9600,
-	19200:   syscall.B19200,
-	38400:   syscall.B38400,
-	57600:   syscall.B57600,
-	115200:  syscall.B115200,
-	230400:  syscall.B230400,
-	460800:  syscall.B460800,
-	500000:  syscall.B500000,
-	576000:  syscall.B576000,
-	921600:  syscall.B921600,
-	1000000: syscall.B1000000,
-	1152000: syscall.B1152000,
-	1500000: syscall.B1500000,
-	2000000: syscall.B2000000,
-	2500000: syscall.B2500000,
-	3000000: syscall.B3000000,
-	3500000: syscall.B3500000,
-	4000000: syscall.B4000000,
+var bauds = map[int]uint64{
+	50:      sio.B50,
+	75:      sio.B75,
+	110:     sio.B110,
+	134:     sio.B134,
+	150:     sio.B150,
+	200:     sio.B200,
+	300:     sio.B300,
+	600:     sio.B600,
+	1200:    sio.B1200,
+	1800:    sio.B1800,
+	2400:    sio.B2400,
+	4800:    sio.B4800,
+	9600:    sio.B9600,
+	19200:   sio.B19200,
+	38400:   sio.B38400,
+	57600:   sio.B57600,
+	115200:  sio.B115200,
+	230400:  sio.B230400,
+	460800:  sio.B460800,
+	500000:  sio.B500000,
+	576000:  sio.B576000,
+	921600:  sio.B921600,
+	1000000: sio.B1000000,
+	1152000: sio.B1152000,
+	1500000: sio.B1500000,
+	2000000: sio.B2000000,
+	2500000: sio.B2500000,
+	3000000: sio.B3000000,
+	3500000: sio.B3500000,
+	4000000: sio.B4000000,
 }
 
 const serve = "The quick brown fox jumps over the lazy dog"
